b3e83d62004ccfda6319a96613f8481b1ff3973a
Loïc NL. NEBONNE <l.nebonne@syntheses-reunion.fr>
2024-04-17 16:12:55 +0400

    first commit

---
+++ src/layouts/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-100 d-flex align-center justify-space-between">
+    <!-- 👉 Footer: left content -->
+    <span class="d-flex align-center">
+      <p class="mb-2 lg:mb-0">© Copyright 2020</p>
+    </span>
+    <!-- 👉 Footer: right content -->
+    <span class="d-md-flex gap-x-4 text-primary d-none">
+      <a href="#" class="mr-6 hover:text-gray-900">Terms of Service</a>
+        <a href="#" class="mr-6 hover:text-gray-900">Privacy Policy</a>
+        <a href="#" class="hover:text-gray-900">About Us</a>
+    </span>
+  </div>
+</template>
