7172c71fb7de66d56c4e4c25407a3aee16cdcfcb
ln549283 <loic.nebonne@gmail.com>
2024-04-17 17:22:51 +0400

    Update index.js

--- src/plugins/router/index.js
+++ src/plugins/router/index.js
@@ -1,12 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from './routes'
-import { publicPath } from '../../vue.config'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  base: publicPath,
-
 })
 
 export default function (app) {
