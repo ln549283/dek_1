48f3bafe41a2b54cab1e617f17692597e6ac7921
ln549283 <loic.nebonne@gmail.com>
2024-04-17 17:06:46 +0400

    Update routes.js

--- src/plugins/router/routes.js
+++ src/plugins/router/routes.js
@@ -5,31 +5,31 @@ export const routes = [
     component: () => import('@/layouts/default.vue'),
     children: [
       {
-        path: 'dashboard',
+        path: '/dashboard',
         component: () => import('@/pages/dashboard.vue'),
       },
       {
-        path: 'account-settings',
+        path: '/account-settings',
         component: () => import('@/pages/account-settings.vue'),
       },
       {
-        path: 'typography',
+        path: '/typography',
         component: () => import('@/pages/typography.vue'),
       },
       {
-        path: 'icons',
+        path: '/icons',
         component: () => import('@/pages/icons.vue'),
       },
       {
-        path: 'cards',
+        path: '/cards',
         component: () => import('@/pages/cards.vue'),
       },
       {
-        path: 'tables',
+        path: '/tables',
         component: () => import('@/pages/tables.vue'),
       },
       {
-        path: 'form-layouts',
+        path: '/form-layouts',
         component: () => import('@/pages/form-layouts.vue'),
       },
     ],
